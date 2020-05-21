74b67a177fef8bb35152294cb2f53498fa9fd274
FT-yuki <yakinikug1010@gmail.com>
2020-05-21 20:37:08 +0900

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -9,58 +9,85 @@ from tqdm import tqdm
 import time
 
 
+def get_movies_info():
+    movies = WebDriverWait(driver, 60).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, 'p-contents-grid'))
+    ).find_elements_by_class_name("js-movie-cassette")
+    movies_info = []
+    for movie in tqdm(movies):
+        movie_info = {}
+        class_names = [x.get_attribute("class") for x in movie.find_element_by_class_name("p-content-cassette__info__main").find_elements_by_tag_name("div")]
+
+        for class_name in class_names:
+            if class_name == "p-content-cassette__action__body":
+                movie_info["watched"] = movie.find_elements_by_class_name(class_name)[0].text
+                movie_info["see_later"] = movie.find_elements_by_class_name(class_name)[1].text
+            elif class_name == "p-content-cassette__title-wrapper":
+                title_info = movie.find_element_by_class_name(class_name)
+                movie_info["title"] = title_info.find_element_by_class_name("p-content-cassette__title").text
+                movie_info["how_to_see"] = title_info.find_element_by_class_name("c-vod-service-types__horizontal").text.split("\n")
+            elif class_name == "p-content-cassette__other-info":
+                outer_info = movie.find_element_by_class_name(class_name)
+                for info in outer_info.find_elements_by_class_name("p-content-cassette__other-info__title"):
+                    if info.text == "上映日：":
+                        movie_info["release_date"] = movie.find_element_by_class_name("p-content-cassette__other-info").find_elements_by_tag_name("span")[0].text
+                    elif info.text == "製作国：":
+                        countrys = movie.find_element_by_class_name("p-content-cassette__other-info").find_elements_by_tag_name("a")
+                        movie_info["country"] = [c.text for c in countrys]
+                    elif info.text == "上映時間：":
+                        movie_info["show_time"] = movie.find_element_by_class_name("p-content-cassette__other-info").find_elements_by_tag_name("span")[-1].text
+            elif class_name == "p-content-cassette__genre":
+                genres = movie.find_elements_by_class_name(class_name)[0].find_elements_by_tag_name("li")
+                movie_info["genre"] = [g.text for g in genres]
+            elif class_name == "c-rating__score":
+                movie_info["rate"] = movie.find_element_by_class_name(class_name).text
+            elif class_name == "p-content-cassette__synopsis-desc__text":
+                movie_info["synopsis"] = movie.find_element_by_class_name(class_name).text
+            elif class_name == "p-content-cassette__people":
+                casts = movie.find_elements_by_class_name(class_name)
+                for cast in casts:
+                    for c in cast.find_elements_by_class_name("p-content-cassette__people-wrap"):
+                        cast_type = c.find_element_by_class_name("p-content-cassette__people-list-term").text
+                        if cast_type == "監督":
+                            movie_info["directer"] = [name.text for name in c.find_elements_by_tag_name("ul")]
+                        elif cast_type == "脚本":
+                            movie_info["writer"] = [name.text for name in c.find_elements_by_tag_name("ul")]
+                        elif cast_type == "出演者":
+                            movie_info["actor"] = [name.text for name in c.find_elements_by_tag_name("ul")]
+        movie_info["best_reviews"] = [x.text for x in movie.find_elements_by_class_name("p-content-cassette__review__text")]
+        movies_info.append(movie_info)
+    return movies_info
+
+
+def crawling(retry_num=5):
+    i = 0
+    while i < retry_num:
+        try:
+            movies_info = get_movies_info()
+        except Exception as e:
+            i = i + 1
+            print("ERROR", e)
+            continue
+        else:
+            return movies_info
+
+
 if __name__ == '__main__':
     driver = webdriver.Chrome(executable_path="./chromedriver")
 
     driver.get("https://filmarks.com/login")
-
     driver.find_elements_by_link_text('Facebookで登録・ログイン')[0].click()
 
     driver.find_element_by_name('email').send_keys("XXXXX@gmail.com")
     driver.find_element_by_name('pass').send_keys("XXXXX")
     driver.find_element_by_id('loginbutton').click()
-    driver.get("https://filmarks.com/list/vod")
-
-    driver.implicitly_wait(5)
-    # 下スクロール => フッタークリック
-    driver.find_elements_by_link_text('動画配信サービスで探す')[0].location_once_scrolled_into_view
-    driver.find_elements_by_link_text('動画配信サービスで探す')[0].click()
-    driver.find_elements_by_link_text('Amazon Prime Videoで鑑賞できる映画')[0].click()
-    
     time.sleep(1)
 
     driver.get("https://filmarks.com/list/vod")
     element = WebDriverWait(driver, 10).until(
 	    EC.visibility_of_element_located((By.LINK_TEXT, 'Amazon Prime Videoで鑑賞できる映画'))
-    movies = WebDriverWait(driver, 60).until(
-	    EC.visibility_of_element_located((By.CLASS_NAME, 'p-contents-grid'))
-    ).find_elements_by_class_name("js-movie-cassette")
-    movies_info = []
-    try:
-        movies = driver.find_elements_by_class_name("js-movie-cassette")
-        for movie in movies:
-            movie_info = {}
-            movie_info["watched"] = movie.find_elements_by_class_name("p-content-cassette__action__body")[0].text
-            movie_info["see_later"] = movie.find_elements_by_class_name("p-content-cassette__action__body")[1].text
-
-            movie_info["title"] = movie.find_elements_by_class_name("p-content-cassette__title")[0].text
-            movie_info["release_date"] = movie.find_element_by_class_name("p-content-cassette__other-info").find_elements_by_tag_name("span")[0].text
-            movie_info["country"] = movie.find_element_by_class_name("p-content-cassette__other-info").find_element_by_tag_name("a").text
-            movie_info["show_time"] = movie.find_element_by_class_name("p-content-cassette__other-info").find_elements_by_tag_name("span")[1].text
-            genres = movie.find_elements_by_class_name("p-content-cassette__genre")[0].find_elements_by_tag_name("li")
-            movie_info["genre"] = [g.text for g in genres]
-            movie_info["rate"] = movie.find_element_by_class_name("c-rating__score").text
-            movie_info["synopsis"] = movie.find_element_by_class_name("p-content-cassette__synopsis-desc__text").text
-            movie_info["directer"] = movie.find_elements_by_class_name("c-label")[0].text
-            movie_info["writer"] = movie.find_elements_by_class_name("c-label")[1].text
-            movie_info["actor"] = [x.text for x in movie.find_elements_by_class_name("p-content-cassette__people-list-desc")[2:]]
-
-            movie_info["how_to_see"] = [x.text for x in movie.find_elements_by_class_name("c-vod-service-types__horizontal")]
-            movie_info["best_reviews"] = [x.text for x in movie.find_elements_by_class_name("p-content-cassette__review__text")]
-
-            movies_info.append(movie_info)
-    except Exception as e:
-        print("time out!", e)
-
-    print("DONE", movies_info)
+    ).click()
+    movies_info = crawling(5)
+    movies_info = pd.DataFrame(movies_info)
+    movies_info.to_csv("./movies_info.csv", encoding="utf-8-sig")
         # TODO #1 続きを見る　の先もクローリング
